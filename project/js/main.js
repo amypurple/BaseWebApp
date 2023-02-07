9a1b637c19c7350c2c402c8f8f0e60bec3b7d438
Amy Bienvenu <amy.bienvenu@outlook.com>
2023-02-06 20:10:19 -0500

    Changes to Amy brand

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,12 @@
-
 function myFunction() {
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
-
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
-
-  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+  var divDemo = document.getElementById("demo");
+  var divImage = document.getElementById("image");
+  divDemo.textContent = "The paragraph changed! #vanilla #javascript";
+  var image = document.createElement('img');
+  image.src="images/amylogo.jpg";
+  image.alt="Amy Bienvenu's Logo";
+  image.height="140";
+  image.width="140";
+  divImage.appendChild(image);
 }
 
